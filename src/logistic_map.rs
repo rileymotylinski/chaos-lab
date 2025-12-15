99176540dcdfe8b345ee8fce1dc2afedc2e5e62c
Riley Motylinski <riley@motylinski.com>
2025-12-15 16:36:17 -0600

    docs: updated docs on logistic_map

--- src/logistic_map.rs
+++ src/logistic_map.rs
@@ -8,16 +8,13 @@
 // attractors vs. repellers
 // repeatedly applying x_{n+1} = rx_n(1-x_n)
 
-use core::borrow;
-use std::borrow::BorrowMut;
-
 /// repeatedly applies the quadratic function
-/// x - starting value
+/// x - starting value. 0 < x < 1
 /// 
 /// n - number of iterations
 /// 
 /// r - constant that modifies the size of the parabola (0.377)
-fn logistic_map(x: f64, n: i64, r: f64) -> Vec<f64> {
+pub fn logistic_map(x: f64, n: i64, r: f64) -> Vec<f64> {
     let mut xprime = r*x*(1.0-x);
     let mut nums = Vec::new();
 
@@ -33,18 +30,30 @@ fn logistic_map(x: f64, n: i64, r: f64) -> Vec<f64> {
 
 
 
-
-pub fn iterative_logistic_map(x: f64, n: i64, start_range: f64, step_size: f64, num_steps: i64) -> Vec<Vec<f64>> {
+/// runs logistic_map over a range of values
+/// 
+/// `x` - initial starting value < 1
+/// 
+/// `n` - number of iterations to run logistic map
+/// 
+/// `start_r` - starting r value (recall: modifies size of parabola)
+/// 
+/// `r_step_size` - amount to increment r by
+/// 
+/// `r_num_steps` - number of times to increase r by
+pub fn iterative_logistic_map(x: f64, n: i64, start_r: f64, r_step_size: f64, r_num_steps: i64) -> Vec<Vec<f64>> {
     let mut logistic_map_outputs = Vec::new();
-    let mut start = start_range;
+    let mut start = start_r;
 
-    for i in (0..num_steps).into_iter() {
+    for i in (0..r_num_steps).into_iter() {
         let output = logistic_map(x, n, start);
+        
+      
         logistic_map_outputs.push(output);
-        println!("{}", start);
-        start += step_size;
+        
+        start += r_step_size;
     }
-
+    
     logistic_map_outputs
 }
 
