9714dc3cfa77f47bf329c97e323a7f53d291681b
Riley Motylinski <riley@motylinski.com>
2025-12-15 16:14:46 -0600

    feat: simplified iterative_logistic_map function logic

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,19 @@
+use crate::logistic_map::iterative_logistic_map;
+
 mod math;
 mod integrators;
 mod logistic_map;
 mod rng;
 mod tests;
 
-use rand::RngCore;
-
-use crate::integrators::euler_step;
-
 fn main() {
-    let data = logistic_map::logistic_data(0.7, 50, 3.7);
-  
-    let mut g = rng::rng_generator(12345);
-    println!("{}",g.next_u32());
-    println!("{}",data[0][0]);
+    let simulation = std::env::args().nth(1).expect("No Simulation was given");
+
+    println!("Simulation selected: {:?}", simulation);
+
+    let _data = iterative_logistic_map(1.0, 50, 3.5, 0.01,50);
 
 
-    let mut state = [1.0]; // inital x (vertical axis) value
-    let t= 0.0; // initial t (horizontal axis) value 
-    let dt = 0.1; // t increment
-
-    // x is like the offset
-    let f = |x: f64, s: &[f64]| -> Vec<f64> {
-        s.iter().map(|n: &f64| -> f64 {x-n}).collect()
-    };
-
-    euler_step(&mut state, t, dt, f);
-
-    print!("{}", state[0]);
 }
 
 
