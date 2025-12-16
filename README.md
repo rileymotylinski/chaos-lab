cb7f98a246b827818ef81c2c6497d7e41d36c137
Riley Motylinski <riley@motylinski.com>
2025-12-15 19:45:10 -0600

    docs: updated README

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 visually simulate chaotic systems in rust.
 e.g. Lorenz attractor, double pendulum, logistic map
 
+## Setup
+
+create a `.csv` in the cwd for logistic map data to write to. Set appropriate `path` varible in `main.rs`
+
 ## To Start
 ```
 ./scripts/start.sh
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use crate::logistic_map::iterative_logistic_map;
+use crate::logistic_map::{iterative_logistic_map, write_logistic_data};
+use std::process;
 
 mod math;
 mod integrators;
@@ -7,13 +8,19 @@ mod rng;
 mod tests;
 
 fn main() {
-    let simulation = std::env::args().nth(1).expect("No Simulation was given");
+    //let simulation = std::env::args().nth(1).expect("No Simulation was given");
 
-    println!("Simulation selected: {:?}", simulation);
+    //println!("Simulation selected: {:?}", simulation);
 
-    let _data = iterative_logistic_map(1.0, 50, 3.5, 0.01,50);
+    let path = "./test.csv";
+    // generating data
+    let data = iterative_logistic_map(0.2,5,3.7, 0.001,5);
 
-
+    // writing data
+    if let Err(err) = write_logistic_data(data, path) {
+        println!("{}", err);
+        process::exit(1);
+    }
 }
 
 
