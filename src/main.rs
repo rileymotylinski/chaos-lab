3eb5fe35fb23bc7bc38601cf8c32648a78540245
Riley Motylinski <riley@motylinski.com>
2025-12-18 10:08:10 -0600

    feat: drop down menu for simulation selecction

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,48 @@ fn main() {
     eframe::run_native("My egui App", native_options, Box::new(|cc| Ok(Box::new(MyEguiApp::new(cc)))));
 }
 
+#[derive(Debug)]
+enum Simulation {
+    Lorenz,
+    Dp,
+}
+
+impl std::default::Default for Simulation {
+    fn default() -> Self {
+        Simulation::Lorenz
+    }
+}
+
+impl PartialEq for Simulation {
+    fn eq(&self, other: &Simulation) -> bool {
+        // matching a tuple of enums
+        // will need to update later
+        match (self,other) {
+            (Simulation::Lorenz, Simulation::Lorenz) => true,
+            (Simulation::Dp, Simulation::Dp) => true,
+            _ => false
+        }
+    }
+}
+
+impl ToString for Simulation {
+    fn to_string(&self) -> String {
+        match self {
+            Simulation::Lorenz => String::from("lorenz"),
+            Simulation::Dp => String::from("double pendulum")
+        }
+    }
+}
+
 #[derive(Default)]
 struct MyEguiApp {
     
+    pub simulation: Simulation,
+
     // lorenz
     pub sigma: f64,
     pub ro: f64,
     pub beta: f64
-
 }
 
 impl MyEguiApp {
@@ -39,30 +73,47 @@ impl MyEguiApp {
         Self::default();
         
         // state is stored int the struct; egui is stateless
-        Self { sigma: 0.0, ro: 0.0, beta: 0.0 }
+        Self { sigma: 0.0, ro: 0.0, beta: 0.0, simulation: Simulation::Lorenz }
     }
+
+
 }
 
 impl eframe::App for MyEguiApp {
 
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-           
-            ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui| {
-                ui.add(egui::Slider::new(&mut self.sigma, 0.0..=100.0));
-                ui.add(egui::Slider::new(&mut self.ro, 0.0..=100.0));
-                ui.add(egui::Slider::new(&mut self.beta, 0.0..=100.0));
-            });
-
-            if ui.button("Run Simulation").clicked() {
-                // lorenz attractor stuff
-                // writing data
-                if let Err(err) = crate::lorenz::lorenz(self.sigma, self.ro,self.beta) {
-                    println!("{}", err);
-                    process::exit(1);
+            
+            egui::ComboBox::from_label("Select one!")
+                .selected_text(self.simulation.to_string())
+                .show_ui(ui, |ui| {
+                    ui.selectable_value(&mut self.simulation, Simulation::Lorenz, Simulation::Lorenz.to_string());
+                    ui.selectable_value(&mut self.simulation, Simulation::Dp, Simulation::Dp.to_string());
                 }
+            );
 
-            }   
+            if (self.simulation == Simulation::Lorenz) {
+                ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui| {
+                    ui.add(egui::Slider::new(&mut self.sigma, 0.0..=100.0));
+                    ui.add(egui::Slider::new(&mut self.ro, 0.0..=100.0));
+                    ui.add(egui::Slider::new(&mut self.beta, 0.0..=100.0));
+                });
+
+                if ui.button("Run Simulation").clicked() {
+                    // lorenz attractor stuff
+                    // writing data
+                    if let Err(err) = crate::lorenz::lorenz(self.sigma, self.ro,self.beta) {
+                        println!("{}", err);
+                        process::exit(1);
+                    }
+
+                }  
+            } else { 
+                ui.heading("This is a double pendulum");
+            }
+            
+
+             
         });
     }
 }
